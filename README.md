c062ad4453ea7d0c79f1a1088d08149d0aedc106
Olubuntu <o.hundogan@gmail.com>
2021-10-13 16:37:58 +0200

    Added todos

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # post_restricted_boltzmann_machine
+
+[] Implement in Torch/Tensorflow
+[] Use on MNIST dataset
+[] Comment on code
+[] Read geoffrey hintons take   
+[] Implement gaussian version
--- restricted_boltzmann_machine.py
+++ restricted_boltzmann_machine.py
@@ -43,8 +43,11 @@ class RBM:
         # data = np.insert(data, 0, 1, axis = 1)
         self.input = input
         if W is None:
-            a = 1. / n_visible
-            init_W = np.random.uniform(low=-a, high=a, size=(n_visible, n_hidden))
+            # Xavier Glorot and Yoshua Bengio            
+            init_W = np.asarray(
+                np.random.uniform(low=-0.1 * np.sqrt(6. / (n_hidden + n_visible)),
+                                  high=0.1 * np.sqrt(6. / (n_hidden + n_visible)),
+                                  size=(n_visible, n_hidden)))
             self.W = init_W
 
         if hbias is None:
@@ -97,13 +100,6 @@ class RBM:
     def sample_h_given_v(self, v):
         h_prob = self.ph_given_v(v)
         h_sampled = (np.random.uniform(size=h_prob.shape) < h_prob) * 1.0
-        # print("SSSSSSSSSSSSSSSS")
-        # print(h_sampled)
-        # h_sampled = np.random.binomial(
-        #     size=h_prob.shape,  # discrete: binomial
-        #     n=1,
-        #     p=h_prob)
-        # print(h_sampled)
         return [h_prob, h_sampled]
 
     def sample_v_given_h(self, h):
@@ -119,13 +115,30 @@ class RBM:
         for epoch in range(epochs or self.epochs):
             self.contrastive_divergence(lr=lr or self.lr, k=k or self.k)
             cost = self.compute_reconstruction_cross_entropy()
-            if (epoch % (epochs//10)) == 0:
+            if (epoch % (epochs // 10)) == 0:
                 print(f'Training epoch {epoch}, cost is {cost}')
+
+    def daydream(self, num_samples=10):
+        # Create a matrix, where each row is to be a sample of of the visible units
+        # (with an extra bias unit), initialized to all ones.
+        samples = np.ones((num_samples, self.n_visible))
+
+        # Take the first sample from a uniform distribution.
+        samples[0] = np.random.rand(self.n_visible)
+
+        v = samples[0, :]
+        for i in range(1, num_samples):
+            v0 = samples[i - 1, :]
+            ph_0, h_0 = self.sample_h_given_v(v0)
+            pv, vk = self.sample_v_given_h(h_0)
+            samples[i, :] = vk
+
+        return samples
 
 
 def test_rbm(lr=0.1, k=5, epochs=2000):
     data = np.array([[1, 1, 1, 0, 0, 0], [1, 0, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 0], [0, 0, 1, 1, 0, 0],
-                     [0, 0, 1, 1, 1, 0], [1, 0, 1, 1, 0, 0]])
+                     [0, 0, 1, 1, 1, 0]])
 
     # construct RBM
     rbm = RBM(input=data, n_visible=6, n_hidden=2)
@@ -143,7 +156,7 @@ def test_rbm(lr=0.1, k=5, epochs=2000):
     print("Reconstruction:")
     print(rbm.reconstruct(v).round(decimals=2))
     print("Reconstruction:")
-    print(rbm.reconstruct(v).round(decimals=2))
+    print(rbm.daydream().round(decimals=2)[1:])
 
 
 test_rbm(epochs=3000)
