37eb0ec10289f1d2f54c140d6f8478cb93baa95c
Olubuntu <o.hundogan@gmail.com>
2021-10-14 11:58:38 +0200

    Impl: debug function

--- BernoulliRBM.py
+++ BernoulliRBM.py
@@ -29,7 +29,7 @@ class RBM:
         W=None,
         hbias=None,
         vbias=None,
-        lr=0.0001,
+        lr=1,
         epochs=1000,
         k=10,
     ) -> None:
@@ -43,7 +43,7 @@ class RBM:
         # data = np.insert(data, 0, 1, axis = 1)
         self.input = input
         if W is None:
-            # Xavier Glorot and Yoshua Bengio            
+            # Xavier Glorot and Yoshua Bengio
             init_W = np.asarray(
                 np.random.uniform(low=-0.1 * np.sqrt(6. / (n_hidden + n_visible)),
                                   high=0.1 * np.sqrt(6. / (n_hidden + n_visible)),
@@ -51,12 +51,16 @@ class RBM:
             self.W = init_W
 
         if hbias is None:
+            # Initial hidden biases of 0 are usually fine. 
             self.hbias = np.random.randn(n_hidden)
 
         if vbias is None:
             self.vbias = np.random.randn(n_visible)
 
-        self.sigma = np.eye(n_hidden) * 1
+        self.collector_Ws = []
+        self.collector_hs = []
+        self.collector_vs = []
+        self.collector_minibatch = []
 
     def sigmoid(self, x):
         # print(x.shape)
@@ -71,17 +75,32 @@ class RBM:
     def contrastive_divergence(self, lr=0.1, k=1, input=None):
         if input is not None:
             self.input = input
-        v_0 = self.input
-        ph_0, h_0 = self.sample_h_given_v(v_0)
+        pv_0 = self.input
+        ph_0, h_0 = self.sample_h_given_v(pv_0)
         h_k = h_0
         for step in range(k):
+            # For the last update of the hidden units, it is silly to use stochastic binary states because nothing depends on which state is chosen.
+            # So use the probability itself to avoid unnecessary sampling noise.
+            # When using CDn, only the final update of the hidden units should use the probability
             pv_k, v_k = self.sample_v_given_h(h_k)
             ph_k, h_k = self.sample_h_given_v(v_k)
 
-        joint_p_vh = ((v_0.T @ ph_0) - (pv_k.T @ ph_k)) / self.num_examples
+
+        # Expectation of (pi x pj) data where pi is the visible unit probability and pj the hidden unit probability (Could also be binary hence E[pi, hj]).
+        # Outer product to get each h and v node combination multiplied
+        pvh_data = (pv_0.T @ ph_0)
+        pvh_recs = (pv_k.T @ ph_k)
+
+        # Division to get expected joint probability of all notes. In Hinton's words: average, per-case gradient computed on a mini-batch
+        joint_p_vh = (pvh_data - pvh_recs) / self.num_examples
         self.W += lr * (joint_p_vh)
-        self.vbias += lr * ((v_0 - pv_k).sum(axis=0) / self.num_examples)
+        self.vbias += lr * ((pv_0 - pv_k).sum(axis=0) / self.num_examples)
         self.hbias += lr * ((ph_0 - ph_k).sum(axis=0) / self.num_examples)
+
+        self.collector_Ws.extend((lr * (joint_p_vh)).flatten())
+        self.collector_vs.extend((lr * ((pv_0 - pv_k).sum(axis=0) / self.num_examples)).flatten())
+        self.collector_hs.extend((lr * ((ph_0 - ph_k).sum(axis=0) / self.num_examples)).flatten())
+        self.collector_minibatch.append(ph_k)
 
     def compute_reconstruction_cross_entropy(self):
         ph = self.ph_given_v(self.input)
@@ -94,15 +113,23 @@ class RBM:
     def reconstruct(self, v):
         ph, h = self.sample_h_given_v(v)
         pv, v = self.sample_v_given_h(h)
-        reconstructed = v
+        reconstructed = pv
         return reconstructed
 
     def sample_h_given_v(self, v):
+        # It is very important to make these hidden states binary, rather than using the probabilities themselves.
         h_prob = self.ph_given_v(v)
-        h_sampled = (np.random.uniform(size=h_prob.shape) < h_prob) * 1.0
+        h_sampled = np.random.binomial(
+            size=h_prob.shape,  # discrete: binomial
+            n=1,
+            p=h_prob)
         return [h_prob, h_sampled]
 
     def sample_v_given_h(self, h):
+        # It is common to use the probability, pi, instead of sampling a binary value. 
+        # This is not nearly as problematic as using probabilities for the data-driven hidden states 
+        # and it reduces sampling noise thus allowing faster learning. 
+        # There is some evidence that it leads to slightly worse density models. 
         v_prob = self.pv_given_h(h)
         v_sampled = np.random.binomial(
             size=v_prob.shape,  # discrete: binomial
@@ -135,29 +162,70 @@ class RBM:
 
         return samples
 
+    def plot_histograms(self):
+        fig, axes = plt.subplots(3,1, figsize=(15,10))
+        ax = axes[0]
+        ax.hist(self.collector_Ws, bins=25)
+        ax.set_title('Weights')
+        ax = axes[1]
+        ax.hist(self.collector_hs, bins=25)
+        ax.set_title('Hidden Bias')
+        ax = axes[2]
+        ax.hist(self.collector_vs, bins=25)
+        ax.set_title('Visible Bias')
+        fig.tight_layout()
+        plt.show()
 
-def test_rbm(lr=0.1, k=5, epochs=2000):
+    def plot_minibatch_probs(self, index = -1):
+        fig, axes = plt.subplots(1,1, figsize=(15,10))
+        data = self.collector_minibatch[index].T
+        y, x = data.shape
+        axes.imshow(data, cmap='gray', vmin=0, vmax=1, origin='lower')
+        axes.set_xlabel('Data Point')
+        axes.set_ylabel('Hidden Unit Activation')
+        axes.set_xticks(range(x))
+        axes.set_yticks(range(y))
+        axes.set_xticklabels(range(1,x+1))
+        axes.set_yticklabels(range(1,y+1))
+        fig.tight_layout()
+        plt.show()
+
+
+def test_rbm(lr=1, k=5, epochs=2000):
     data = np.array([[1, 1, 1, 0, 0, 0], [1, 0, 1, 0, 0, 0], [1, 1, 1, 0, 0, 0], [0, 0, 1, 1, 1, 0], [0, 0, 1, 1, 0, 0],
-                     [0, 0, 1, 1, 1, 0]])
+                     [0, 0, 1, 1, 1, 0], [0, 0, 1, 0, 1, 0], [1, 0, 1, 0, 0, 0], [0, 0, 1, 1, 0, 0]])
 
     # construct RBM
-    rbm = RBM(input=data, n_visible=6, n_hidden=2)
+    rbm = RBM(input=data, n_visible=6, n_hidden=4)
 
     # train
     rbm.run_train_loop(epochs, lr, k)
 
     # test
-    v = np.array([[0, 0, 0, 1, 1, 0], [1, 1, 0, 0, 0, 0], [1, 1, 0, 0, 0, 1], [0, 0, 1, 0, 1, 0]])
 
     print("Data:")
     print(data)
-    print("Test:")
-    print(v)
-    print("Reconstruction:")
-    print(rbm.reconstruct(v).round(decimals=2))
-    print("Reconstruction:")
-    print(rbm.daydream().round(decimals=2)[1:])
+
+    return rbm
 
 
-test_rbm(epochs=3000)
+
+
+
+
+rbm = test_rbm(lr=1, epochs=2000, k=20)
 # %%
+v = np.array([[0, 0, 0, 1, 1, 0], [1, 1, 0, 0, 0, 0], [1, 1, 0, 0, 0, 1], [0, 0, 1, 0, 1, 0]])
+print("Test:")
+print(v)
+print("Reconstruction:")
+print(rbm.reconstruct(v).round(decimals=2))
+
+# %%
+rbm.plot_minibatch_probs(3)
+# %%
+rbm.plot_minibatch_probs(10)
+# %%
+rbm.plot_minibatch_probs(50)
+# %%
+rbm.plot_minibatch_probs()
--- README.md
+++ README.md
@@ -1,7 +1,12 @@
 # post_restricted_boltzmann_machine
 
-- [ ] Implement in Torch/Tensorflow
-- [ ] Use on MNIST dataset
+- [x] Implement basic version
+- [x] Read geoffrey hintons take 
+- [x] Implement debug functions
+- [ ] Implement functional version
 - [ ] Comment on code
-- [ ] Read geoffrey hintons take   
-- [ ] Implement gaussian version
+- [ ] Use on MNIST dataset
+- [ ] Add L2/L1 regularisation 
+- [ ] Add compute average free energy function
+- [?] Implement gaussian version
+- [ ] Implement in Torch/Tensorflow
